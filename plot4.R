b8152c804e2a08b4c3a1bd7c21cbba35d572cfdf
bitaolc <manjusaka89@gmail.com>
2018-03-29 18:12:53 -0400

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,31 @@
+library(pryr)
+setwd("E:/Folder/Coursera/R/Exploratory Data Analysis/Week1")
+
+#download data
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+if (file.exists("dataset.zip") == F){
+download.file(url, "dataset.zip", method = "auto")}
+unzip("dataset.zip")
+
+#read data
+hpc_raw <- read.table("household_power_consumption.txt", header = T, sep = ";", stringsAsFactors = F)
+
+#check dim and data frame size
+dim(hpc_raw) 
+object_size(hpc_raw)
+
+#clean and format dataset
+hpc <- hpc_raw[which(hpc_raw$Date %in% c("1/2/2007","2/2/2007")),]
+hpc[] <- lapply(hpc, function(x) gsub("\\?", NA, x)) #replace hpc with NA
+
+#plot4
+png("plot4.png", width=480, height=480)
+par(mfrow = c(2,2))
+plot(hpc$Day, as.numeric(hpc$Global_active_power), type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+plot(hpc$Day, as.numeric(hpc$Voltage), type = "l", xlab = "datetime", ylab = "Voltage")
+plot(hpc$Day, as.numeric(hpc$Sub_metering_1), type = "s", xlab = "", ylab = "Energy sub metering")
+lines(hpc$Day, as.numeric(hpc$Sub_metering_2), type = "s", col="red", xlab = "", ylab = "Energy sub metering")
+lines(hpc$Day, as.numeric(hpc$Sub_metering_3), type = "s", col="blue", xlab = "", ylab = "Energy sub metering")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black","red","blue"), lty=1)
+plot(hpc$Day, as.numeric(hpc$Global_reactive_power), type = "l", xlab = "", ylab = "Global_reactive_power")
+dev.off()
